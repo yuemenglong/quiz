44c956da178f331f1ab3d52e6077d0dcd46aa943
yuemenglong <yuemenglong@126.com>
2017-07-31 15:20:15 +0800

    add: 配置db

--- src/main/java/yy/rdpac/bean/Dao.scala
+++ src/main/java/yy/rdpac/bean/Dao.scala
@@ -2,6 +2,7 @@ package yy.rdpac.bean
 
 import javax.annotation.PostConstruct
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import yy.json.JSON
 import yy.orm.Orm
@@ -16,10 +17,21 @@ class Dao {
 
   var db: Db = _
 
+  @Value("${db.host}")
+  var host: String = _
+  @Value("${db.port}")
+  var port: Integer = _
+  @Value("${db.user}")
+  var user: String = _
+  @Value("${db.pwd}")
+  var pwd: String = _
+  @Value("${db.database}")
+  var database: String = _
+
   @PostConstruct
   def init(): Unit = {
     Orm.init("yy.rdpac.entity")
-    db = Orm.openDb("localhost", 3306, "root", "root", "rdpac")
+    db = Orm.openDb(host, port, user, pwd, database)
     JSON.setConstructorMap(Orm.getEmptyConstructorMap)
   }
 
