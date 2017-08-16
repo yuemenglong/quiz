7b9fd8cbd6f91d33ed7275d46e3c758c966487cb
yuemenglong <yuemenglong@126.com>
2017-08-16 14:31:21 +0800

    add: 修复线上数据错误

--- src/main/java/yy/rdpac/entity/Question.scala
+++ src/main/java/yy/rdpac/entity/Question.scala
@@ -1,5 +1,6 @@
 package yy.rdpac.entity
 
+import io.github.yuemenglong.orm.Orm
 import io.github.yuemenglong.orm.lang.anno._
 import io.github.yuemenglong.orm.lang.types.Types
 import io.github.yuemenglong.orm.lang.types.Types._
@@ -90,7 +91,7 @@ class Mark {
   var id: Long = _
   var infoId: Long = _
   @Pointer
-  var info:Question = _
+  var info: Question = _
   var userId: Long = _
 }
 
@@ -112,4 +113,32 @@ class Study {
   @Pointer
   var quiz: Quiz = _
   var quizId: Long = _
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    Orm.init("yy.rdpac.entity")
+    val db = Orm.openDb("211.159.173.48", 3306, "work", "work", "rdpac")
+    db.beginTransaction(session => {
+      val r1 = Orm.root(classOf[Mark]).asSelect()
+      val q1 = Orm.select(r1).from(r1).where(r1.get("userId").eql(new Integer(2)))
+      val marks = session.query(q1)
+      val infoIds = marks.map(_.infoId.asInstanceOf[Object])
+      val r2 = Orm.root(classOf[QuizQuestion]).asSelect()
+      val q2 = Orm.select(r2).from(r2).where(r2.get("id").in(infoIds))
+      val qqs = session.query(q2)
+      val qqMap: Map[Long, QuizQuestion] = qqs.map(qq => (qq.id, qq))(collection.breakOut)
+      marks.foreach(mark => {
+        val infoId = mark.infoId
+        if (qqMap.contains(infoId)) {
+          val realId = qqMap(infoId).infoId
+          println(s"${infoId} => ${realId}")
+        } else {
+          println(s"Not Found: ${infoId}")
+        }
+      })
+      //      marks.foreach(println)
+      //      qqs.foreach(println)
+    })
+  }
 }
